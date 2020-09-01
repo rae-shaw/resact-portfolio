fe92b5ffaa0535c1034d11289fb1ede6c4b95b11
Rachael Shaw <rachaelleeshaw@gmail.com>
2020-09-01 10:07:36 -0500

    routes first draft

---
+++ src/NotFound/NotFound.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Nav from '../Nav/Nav.js'
+import Footer from '../Footer/Footer.js'
+
+
+export default function Resume() {
+	return (
+		<section>
+			<Nav />
+			<main>
+	      <h1>This is not the page you are looking for... </h1>
+	    </main>
+	  	<Footer />
+  	</section>
+	)
+}
